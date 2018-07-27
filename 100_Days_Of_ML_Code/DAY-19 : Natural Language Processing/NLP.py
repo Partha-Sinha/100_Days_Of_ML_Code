9f7c1373461d415c3c9fa2a7cddace843e90af3c
Partha-sinha <schrodinger.zerf@gmail.com>
2018-07-27 15:19:09 +0530

    Day-19 Natural Language Processing

--- 100_Days_Of_ML_Code/DAY-19 : Natural Language Processing/NLP.py
+++ 100_Days_Of_ML_Code/DAY-19 : Natural Language Processing/NLP.py
@@ -12,3 +12,4 @@ dataset = pd.read_csv('Restaurant_Reviews.tsv', delimiter = '\t', quoting = 3)
 # Cleaning the texts
 import re
 review = re.sub('[^a-zA-Z]', ' ', dataset['Review'][0])
+review = review.lower()
