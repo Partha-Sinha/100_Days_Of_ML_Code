7db7e103a98bbf8c1eb7e1aabdc6f926fe5d4032
Partha-sinha <schrodinger.zerf@gmail.com>
2018-08-12 22:36:25 +0530

    Day 31-Recurrent Neural Networks

--- 100_Days_Of_ML_Code/DAY-30, 31 : RNN/RNN.py
+++ 100_Days_Of_ML_Code/DAY-30, 31 : RNN/RNN.py
@@ -17,3 +17,13 @@ training_set = sc.fit_transform(training_set)
 # Getting the inputs and the outputs
 X_train = training_set[0:1257]
 y_train = training_set[1:1258]
+
+# Reshaping
+X_train = np.reshape(X_train, (1257, 1, 1))
+
+# Importing the Keras Libraries and Packages
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
+
+# Initialising the RNN
+regressor = Sequential()
