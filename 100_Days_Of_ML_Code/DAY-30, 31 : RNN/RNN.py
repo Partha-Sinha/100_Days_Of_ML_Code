368c9a8a57e08e0dec295804253c18ba27533d73
Partha-sinha <schrodinger.zerf@gmail.com>
2018-08-12 21:47:20 +0530

    Day 31-Recurrent Neural Networks

--- 100_Days_Of_ML_Code/DAY-30, 31 : RNN/RNN.py
+++ 100_Days_Of_ML_Code/DAY-30, 31 : RNN/RNN.py
@@ -8,3 +8,8 @@ import pandas as pd
 # Importing the Training set
 training_set = pd.read_csv('Google_Stock_Price_Train.csv')
 training_set = training_set.iloc[:, 1:2].values
+
+# Feature Scaling
+from sklearn.preprocessing import MinMaxScaler
+sc = MinMaxScaler()
+training_set = sc.fit_transform(training_set)
