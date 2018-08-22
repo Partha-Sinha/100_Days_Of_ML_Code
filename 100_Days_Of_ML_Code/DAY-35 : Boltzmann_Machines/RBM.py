bc3f30a55aa40ec6ff98e1eff881601cc231d903
Partha-sinha <schrodinger.zerf@gmail.com>
2018-08-22 10:39:24 +0530

    Day 35 - Boltzmann Machines

--- 100_Days_Of_ML_Code/DAY-35 : Boltzmann_Machines/RBM.py
+++ 100_Days_Of_ML_Code/DAY-35 : Boltzmann_Machines/RBM.py
@@ -16,3 +16,7 @@ users = movies = pd.read_csv('ml-1m/users.dat', sep = '::', header = None, engin
 ratings = movies = pd.read_csv('ml-1m/ratings.dat', sep = '::', header = None, engine = 'python', encoding = 'latin-1')
 
 # Preparing the Training set and Test set
+training_set = pd.read_csv('ml-100k/u1.base', delimiter = '\t')
+training_set = np.array(training_set, dtype = 'int')
+test_set = pd.read_csv('ml-100k/u1.test', delimiter = '\t')
+test_set = np.array(test_set, dtype = 'int')
