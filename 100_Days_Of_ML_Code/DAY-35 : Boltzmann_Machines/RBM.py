db6a857b25040c29bb90975fa7785542786133fc
Partha-sinha <schrodinger.zerf@gmail.com>
2018-08-23 14:39:58 +0530

    Day 36 - Boltzmann Machines

--- 100_Days_Of_ML_Code/DAY-35 : Boltzmann_Machines/RBM.py
+++ 100_Days_Of_ML_Code/DAY-35 : Boltzmann_Machines/RBM.py
@@ -58,6 +58,24 @@ class RBM():
         self.W = torch.randn(nh, nv)
         self.a = torch.randn(1, nh)
         self.b = torch.randn(1, nv)
+    def sample_h(self, x):
+        wx = torch.mm(x, self.W.t())
+        activation = wx + self.a.expand_as(wx)
+        P_h_given_v = torch.sigmoid(activation)
+        return P_h_given_v, torch.bernoulli(P_h_given_v)
+    def sample_v(self, y):
+        wy = torch.mm(y, self.W)
+        activation = wy + self.b.expand_as(wy)
+        P_v_given_v = torch.sigmoid(activation)
+        return P_v_given_v, torch.bernoulli(P_v_given_v)
+    def train(self, v0, vk, ph0, phk):
+        self.W += torch.mm(v0.t(), ph0) - torch.mm(vk.t(), phk)
+        self.b += torch.sum((v0 - vk), 0)
+        self.a += torch.sum((ph0 - phk), 0)
+nv = 
+        
+        
+        
 
 
 
