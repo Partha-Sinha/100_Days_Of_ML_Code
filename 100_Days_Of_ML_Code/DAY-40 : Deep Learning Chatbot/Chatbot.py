da9988ee209853356ba45f1ea24278822f7f0398
Partha-sinha <schrodinger.zerf@gmail.com>
2018-09-05 23:38:06 +0530

    Day 40 - Deep Learning Chatbot

--- 100_Days_Of_ML_Code/DAY-40 : Deep Learning Chatbot/Chatbot.py
+++ 100_Days_Of_ML_Code/DAY-40 : Deep Learning Chatbot/Chatbot.py
@@ -69,6 +69,64 @@ clean_answers = []
 for answer in answers:
     clean_answers.append(clean_text(answer))
 
+# Creating a dictionary that maps each word to its numbers of occurences
+word2count = {}
+for question in clean_questions:
+    for word in question.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+            
+
+for answer in clean_answers:
+    for word in answer.split():
+        if word not in word2count:
+            word2count[word] = 1
+        else:
+            word2count[word] += 1
+            
+# Creating two dictionaries that map the questions words and the answers to a unique interger
+threshold = 20
+questionswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        questionswords2int[word] = word_number
+        word_number += 1
+
+
+answerswords2int = {}
+word_number = 0
+for word, count in word2count.items():
+    if count >= threshold:
+        answerswords2int[word] = word_number
+        word_number += 1
+
+# Adding the last tokens to these two dictionaries
+tokens = ['<PAD>', '<EOS>', '<OUT>', '<SOS>']
+for token in tokens:
+    questionswords2int[token] = len(questionswords2int) + 1
+    
+for token in tokens:
+    answerswords2int[token] = len(answerswords2int) + 1
+
+# Creating the inverse dictionary of the answerswords2int dictionary
+answersints2word = {w_i: w for w, w_i in answerswords2int.items()}
+
+# Adding the End of String token to the end of every answers
+for i in range(len(clean_answers)):
+    clean_answers[i] += ' <EOS>'
+
+
+
+
+
+    
+
+        
+
+
 
 
 
